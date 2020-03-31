ffc7fd9f9fa2e874466b3ae92ad356a1fc60e6b9
Wim Wauters <wauters1978@gmail.com>
2020-03-31 21:41:47 +0200

    Initial commit

--- Create_VM_on_vSphere_Terraform/variables.tf
+++ Create_VM_on_vSphere_Terraform/variables.tf
@@ -1,81 +1,63 @@
-
 variable "vsphere_server" {
-  type    = string
-  default = "10.16.2.99"
+  default = "10.x.y.z"
 }
 
 variable "vsphere_user" {
-  type    = string
   default = "administrator@vsphere.local"
 }
 
 variable "vsphere_password" {
-  type    = string
-  default = "---"
+  default = "***"
 }
 
 variable "vsphere_datacenter" {
-  type    = string
   default = "SaS-DC"
 }
 
 variable "vsphere_datastore" {
-  type    = string
   default = "datastore-UCS-POD1-2"
 }
 
-
 variable "vsphere_compute_cluster" {
-  type    = string
   default = "SaS-Cluster"
 }
 
 variable "vsphere_template" {
-  type    = string
   default = "ubuntu-1604-server-template"
 }
 
 variable "folder" {
-  type    = string
   default = "wauterw"
 }
 
 variable "aci_vm1_name" {
-  type    = string
-  default = "ACI1-noACI"
+  default = "vSphere1"
 }
 
 variable "aci_vm2_name" {
-  type    = string
-  default = "ACI2-noACI"
+  default = "vSphere2"
 }
 
 variable "aci_vm1_address" {
-  type    = string
   default = "10.16.2.233"
 }
 
 variable "aci_vm2_address" {
-  type    = string
   default = "10.16.2.234"
 }
 
 variable "gateway" {
-  type    = string
   default = "10.16.2.254"
 }
 
 variable "dns_list" {
-  type    = string
   default = "10.9.15.1"
 }
 
 variable "dns_search" {
-  type    = string
   default = "cisco.com"
 }
 
 variable "domain_name" {
-  type    = string
   default = "cisco.com"
 }
---
+++ DigitalOcean_Terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "do_token" {
+  default = "82c33fc63241c7adeab8a4deae5da397acbcb2c2c63d2ea08f1089bd6522f56d"
+}
+
+variable "region" {
+  default = "AMS3"
+}
+
+variable "ssh_fingerprint" {
+  default =  "5a:25:96:d4:b5:40:d4:49:b3:0d:3b:d3:b2:c9:8d:03"
+}
+
+
+variable "number_servers" {
+  default = "2"
+}
