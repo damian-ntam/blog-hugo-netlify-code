c4695b6d188633304a14b3e20dd7a5d272995041
Wim Wauters <wauters1978@gmail.com>
2020-04-01 14:58:49 +0200

    Initial commit

--- AWS_Terraform/main.tf
+++ AWS_Terraform/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "OneServer" {
+resource "aws_instance" "Server" {
   ami           = var.ec2_image
   instance_type = var.ec2_instance_type
   key_name      = var.ec2_keypair
@@ -9,10 +9,10 @@ resource "aws_instance" "OneServer" {
 }
 
 output "instance_ip_addr" {
-  value       = aws_instance.OneServer.*.private_ip
+  value       = aws_instance.Server.*.private_ip
   description = "The private IP address of the main server instance."
 }
 
 output "instance_ips" {
-  value = aws_instance.OneServer.*.public_ip
+  value = aws_instance.Server.*.public_ip
 }
---
+++ DigitalOcean_Kubernetes_Ansible/Terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "do_token" {
+  default = "ce93***e7"
+}
+
+variable "region" {
+  default = "AMS3"
+}
+
+variable "ssh_fingerprint" {
+  default =  "5a:25:***:c9:8d:03"
+}
+
+
+variable "number_servers" {
+  default = "3"
+}
