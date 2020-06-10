6a438d4d03d42b3ec39e47a13971fefc9a551e99
Wim Wauters <wauters1978@gmail.com>
2020-06-10 15:41:44 +0200

    Initial commit

--- RestConf_Python/get_static_routes_ietf.py
+++ RestConf_Python/get_static_routes_ietf.py
@@ -15,19 +15,17 @@ headers = {
       "Content-Type" : "application/yang-data+json", 
    }
 
-module = "ietf-interfaces:interfaces"
+module = "ietf-routing:routing/routing-instance=default/routing-protocols/routing-protocol=static,1"
 
 url = f"https://{device['ip']}:{device['port']}/restconf/data/{module}"
-print(url)
 
 requests.packages.urllib3.disable_warnings()
 response = requests.get(url, headers=headers, auth=(device['username'], device['password']), verify=False).json()
 
-interfaces = response['ietf-interfaces:interfaces']['interface']
+routes = response['ietf-routing:routing-protocol']['static-routes']
 
-for interface in interfaces:
-   if bool(interface['ietf-ip:ipv4']):
-        print(f"{interface['name']} -- {interface['description']} -- {interface['ietf-ip:ipv4']['address'][0]['ip']}")
+for route in routes['ietf-ipv4-unicast-routing:ipv4']['route']:
+    print(f"Route: {route['destination-prefix']} with {route['next-hop']['outgoing-interface']}" )
 
-  
+
       
